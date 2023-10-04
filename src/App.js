d23db7932ad9a6b71b36469c5e95906322561ed9
calebcianc <calebcc.castro@gmail.com>
2023-10-04 22:59:50 +0800

    added color scheme to app.css file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <body className="App-body">
+        <img src="/logo512.png" className="App-logo" alt="logo" />
+        <h1>Welcome to ChefTalk</h1>
+      </body>
     </div>
   );
 }
