d7c9c4e445635d2d8076ee91d9104b604245f827
khloeli <chloelihuimin@gmail.com>
2023-10-05 09:03:24 +0800

    add first draft of folder structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,65 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { useNavigate, Routes, Route, Link } from "react-router-dom";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import PublicIcon from "@mui/icons-material/Public";
+import HomeIcon from "@mui/icons-material/Home";
+import PersonIcon from "@mui/icons-material/Person";
+import HomePage from "./Pages/HomePage";
+import ErrorPage from "./Pages/ErrorPage";
+import ExplorePage from "./Pages/ExplorePage";
+import ProfilePage from "./Pages/ProfilePage";
 
 function App() {
+  const [value, setValue] = React.useState(0);
+  const navigate = useNavigate();
   return (
     <div className="App">
       <body className="App-body">
-        <img src="/logo512.png" className="App-logo" alt="logo" />
-        <h1>Welcome to ChefTalk</h1>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <img src="/logo512.png" className="App-logo" alt="logo" />
+                <h1>Welcome to ChefTalk</h1>
+                <HomePage />
+              </>
+            }
+          />
+          <Route path="/explore" element={<ExplorePage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
       </body>
+      <Box>
+        <BottomNavigation
+          showLabels
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+            switch (newValue) {
+              case 0:
+                navigate("/explore");
+                break;
+              case 1:
+                navigate("/");
+                break;
+              case 2:
+                navigate("/profile");
+                break;
+              default:
+                break;
+            }
+          }}
+        >
+          <BottomNavigationAction label="Explore" icon={<PublicIcon />} />
+          <BottomNavigationAction label="Home" icon={<HomeIcon />} />
+          <BottomNavigationAction label="Profile" icon={<PersonIcon />} />
+        </BottomNavigation>
+      </Box>
     </div>
   );
 }
