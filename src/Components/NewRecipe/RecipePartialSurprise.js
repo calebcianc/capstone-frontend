577ae40aef18da8a0fa1fa933d5385c9b4fd6a82
calebcianc <calebcc.castro@gmail.com>
2023-10-09 23:43:29 +0800

    added state mgmt to the recipe generator modal

--- src/Components/NewRecipe/RecipePartialSurprise.js
+++ src/Components/NewRecipe/RecipePartialSurprise.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
+  Box,
   Button,
   Dialog,
   DialogTitle,
@@ -7,13 +8,33 @@ import {
   DialogActions,
   TextField,
   MenuItem,
+  ButtonGroup,
+  Input,
+  InputAdornment,
+  IconButton,
+  Typography,
 } from "@mui/material";
+import { AddCircleOutline, RemoveCircleOutline } from "@mui/icons-material";
 
 export default function RecipePartialSurprise() {
   const [open, setOpen] = useState(false);
+  const [mealType, setMealType] = useState("");
+  const [cuisineType, setCuisineType] = useState("");
+  const [dietaryRestrictions, setDietaryRestrictions] = useState("none");
+  const [servings, setServings] = useState(1);
+  const [prepTime, setPrepTime] = useState("30min");
+
+  useEffect(() => {
+    console.log(mealType, cuisineType, dietaryRestrictions, servings, prepTime);
+  }, [mealType, cuisineType, dietaryRestrictions, servings, prepTime]);
 
   const handleClose = () => {
     setOpen(false);
+    setMealType("");
+    setCuisineType("");
+    setDietaryRestrictions("none");
+    setServings(1);
+    setPrepTime("30min");
   };
 
   return (
@@ -34,63 +55,147 @@ export default function RecipePartialSurprise() {
         </DialogTitle>
 
         <DialogContent style={{ backgroundColor: "#f7f4e8" }}>
+          {/* Meal Type */}
           <TextField
             select
             fullWidth
             margin="dense"
             label="Meal Type"
             variant="outlined"
-            style={{ backgroundColor: "#d5d4d0", borderRadius: "16px" }}
+            value={mealType}
+            onChange={(e) => setMealType(e.target.value)}
+            style={{ backgroundColor: "#d5d4d0" }}
           >
-            {/* Add options as necessary */}
             <MenuItem value="breakfast">Breakfast</MenuItem>
             <MenuItem value="lunch">Lunch</MenuItem>
             <MenuItem value="dinner">Dinner</MenuItem>
           </TextField>
 
+          {/* Cuisine Type */}
           <TextField
             select
             fullWidth
             margin="dense"
             label="Cuisine Type"
             variant="outlined"
-            style={{ backgroundColor: "#d5d4d0", borderRadius: "16px" }}
+            value={cuisineType}
+            onChange={(e) => setCuisineType(e.target.value)}
+            style={{ backgroundColor: "#d5d4d0" }}
           >
-            {/* Add options as necessary */}
             <MenuItem value="italian">Italian</MenuItem>
             <MenuItem value="chinese">Chinese</MenuItem>
+            <MenuItem value="japanese">Japanese</MenuItem>
+            <MenuItem value="mexican">Mexican</MenuItem>
+            <MenuItem value="french">French</MenuItem>
+            <MenuItem value="indian">Indian</MenuItem>
+            <MenuItem value="thai">Thai</MenuItem>
+            <MenuItem value="spanish">Spanish</MenuItem>
+            <MenuItem value="korean">Korean</MenuItem>
+            <MenuItem value="american">American</MenuItem>
           </TextField>
 
+          {/* Dietary Restrictions */}
           <TextField
             select
             fullWidth
             margin="dense"
             label="Dietary Restrictions"
             variant="outlined"
-            style={{ backgroundColor: "#d5d4d0", borderRadius: "16px" }}
+            value={dietaryRestrictions}
+            onChange={(e) => setDietaryRestrictions(e.target.value)}
+            style={{ backgroundColor: "#d5d4d0" }}
           >
-            {/* Add options as necessary */}
+            <MenuItem value="none">None</MenuItem>
             <MenuItem value="vegetarian">Vegetarian</MenuItem>
             <MenuItem value="vegan">Vegan</MenuItem>
+            <MenuItem value="gluten-free">Gluten-Free</MenuItem>
+            <MenuItem value="dairy-free">Dairy-Free</MenuItem>
+            <MenuItem value="nut-free">Nut-Free</MenuItem>
+            <MenuItem value="halal">Halal</MenuItem>
+            <MenuItem value="kosher">Kosher</MenuItem>
+            <MenuItem value="paleo">Paleo</MenuItem>
+            <MenuItem value="keto">Keto</MenuItem>
+            <MenuItem value="low-carb">Low Carb</MenuItem>
           </TextField>
 
-          <TextField
-            fullWidth
-            margin="dense"
-            type="number"
-            label="Number of Servings"
-            variant="outlined"
-            style={{ backgroundColor: "#d5d4d0", borderRadius: "16px" }}
-          />
+          <Typography
+            variant="h7"
+            style={{ color: "#2b2b2b", marginBottom: 8 }}
+          >
+            Serving Size
+          </Typography>
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            {[1, 2, 3, 4, 5].map((num) => (
+              <Button
+                key={num}
+                variant="outlined"
+                style={{
+                  color: "#2b2b2b",
+                  borderColor: "#2b2b2b",
+                  borderRadius: "16px",
+                  flex: 1,
+                  margin: 4,
+                }}
+                onClick={() => {
+                  setServings(num);
+                }}
+              >
+                {num}
+              </Button>
+            ))}
+            <Input
+              type="number"
+              inputProps={{ min: 6 }}
+              style={{
+                backgroundColor: "#d5d4d0",
+                borderRadius: "16px",
+                width: 60,
+                marginLeft: 4,
+              }}
+              onChange={(e) => {
+                const value = parseInt(e.target.value, 10);
+                if (value > 5) {
+                  setServings(value);
+                }
+              }}
+            />
+          </Box>
 
-          <TextField
-            fullWidth
-            margin="dense"
-            type="number"
-            label="Preparation Time (in minutes)"
-            variant="outlined"
-            style={{ backgroundColor: "#d5d4d0", borderRadius: "16px" }}
-          />
+          <Typography
+            variant="h7"
+            style={{ color: "#2b2b2b", marginBottom: 8 }}
+          >
+            Preparation Time
+          </Typography>
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+            mt={2}
+          >
+            {["15min", "30min", "1hr", "1.5hr", "2hr"].map((label) => (
+              <Button
+                key={label}
+                variant="outlined"
+                style={{
+                  color: "#2b2b2b",
+                  borderColor: "#2b2b2b",
+                  borderRadius: "16px",
+                  flex: 1,
+                  margin: 4,
+                }}
+                onClick={() => {
+                  setPrepTime(label);
+                }}
+              >
+                {label}
+              </Button>
+            ))}
+          </Box>
         </DialogContent>
 
         <DialogActions
@@ -103,7 +208,7 @@ export default function RecipePartialSurprise() {
             onClick={handleClose}
             variant="contained"
             style={{
-              backgroundColor: "#81c545",
+              backgroundColor: "#2b2b2b",
               color: "#f7f4e8",
               borderRadius: "16px",
             }}
