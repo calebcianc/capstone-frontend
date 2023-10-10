1bbdf921a67758bbb877985ca69d53bcf2062a89
KenHo95 <mingquan.ho.2017@accountancy.smu.edu.sg>
2023-10-11 00:21:25 +0800

    Added functionality to fetch recipe ingredient and instructions to recipe page

--- src/Components/Recipe/RecipePage.js
+++ src/Components/Recipe/RecipePage.js
@@ -1,24 +1,44 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 
 export default function RecipePage() {
   const { recipeId } = useParams();
-  const [recipe, setRecipe] = useState(null);
 
   // useEffect to fetch recipe from postgres db by recipeId
+  const [recipe, setRecipe] = useState([]);
   useEffect(() => {
-    const fetchedRecipe = "test";
-    // fetchRecipe(recipeId);
-    setRecipe(fetchedRecipe);
-  }, [recipeId]);
+    fetchRecipe();
+  }, []);
+
+  const fetchRecipe = async () => {
+    const fetchedRecipe = await axios.get(
+      `http://localhost:3001/recipes/${recipeId}`
+    );
+    setRecipe(fetchedRecipe.data[0]);
+  };
+
+  const ingredients = recipe.ingredients?.map((ingredient, ind) => {
+    return <li>{ingredient.name}</li>;
+  });
+
+  const instructions = recipe.instructions?.map((instruction, ind) => {
+    return <li>{instruction.instruction} </li>;
+  });
 
   if (!recipe) return <div>Loading...</div>;
 
   return (
     <div>
-      <h1>{recipe.title}</h1>
-      <img src={recipe.photoUrl} alt={recipe.title} />
+      {/* {console.log(recipe)} */}
+      <h1>{recipe.name}</h1>
+      {/* <img src={recipe.photoUrl} alt={recipe.name} /> */}
       {/* ... display ingredients and instructions ... */}
+      Ingredients:
+      <ol>{ingredients}</ol>
+      <br />
+      Instructions:
+      <ol>{instructions}</ol>
     </div>
   );
 }
